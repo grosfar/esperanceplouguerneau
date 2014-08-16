9aa61c382046528e5c3922e70fa21e03a825b73a
grosfar <raphael.corre@gmail.com>
2014-08-16 21:53:01 +0200

    Changement de la couleur verte dans LESS

--- index.php
+++ index.php
@@ -67,12 +67,25 @@ $sitename = $app->getCfg('sitename');
 			<div class="row" id="accueil" name="accueil">
 				<br>
 				<div class="col-md-3 hidden-sm hidden-xs">
-					<p><img src="<?php echo $this->baseurl ?>/images/logo-esp.jpg"/></p>
-					<p style="margin-top:10px"><script src="http://connect.facebook.net/fr_FR/all.js#xfbml=1"></script><fb:like href="http://www.esperanceplouguerneau.fr/" show_faces="true" width="200px"></fb:like></p>
+					<div class="panel panel-success">
+						<div class="panel-heading">
+							<h3 class="panel-title">&nbsp;</h3>
+						</div>
+						<div class="panel-body">
+							<p><img src="<?php echo $this->baseurl ?>/images/logo-esp.jpg"/></p>
+							<p style="margin-top:10px"><script src="http://connect.facebook.net/fr_FR/all.js#xfbml=1"></script><fb:like href="http://www.esperanceplouguerneau.fr/" show_faces="true" width="200px"></fb:like></p>
+						</div>
+					</div>
 				</div>
 				<div class="col-xs-1 hidden-sm hidden-md hidden-lg">
 				</div>
 				<div class="col-md-6 col-sm-8 col-xs-10">
+					<div class="alert alert-success" role="alert">
+						<button type="button" class="close" data-dismiss="alert">
+							<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+						</button>
+						Pour cette nouvelle saison 2014/2015, le comité directeur de l'Espérance a décidé de mettre en place une "opération spéciale équipements". Cette opération consiste à proposer un "pack équipements" à un tarif très préférentiel. En effet, le coût d'achat de ce pack est de 70 euros et, sera vendu seulement 30 euros aux licenciés. L'Espérance prenant à sa charge 60% du prix.
+					</div>
 					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 						<ol class="carousel-indicators">
 							<li class="active" data-target="#carousel-example-generic" data-slide-to="0"></li>
@@ -91,15 +104,21 @@ $sitename = $app->getCfg('sitename');
 					</div>
 				</div>
 				<div class="col-md-3 col-sm-4 hidden-xs">
-					<jdoc:include type="modules" name="position-10"/>
+					<div class="panel panel-success">
+						<div class="panel-heading">
+							<h3 class="panel-title">Nos partenaires</h3>
+						</div>
+						<div class="panel-body">
+							<jdoc:include type="modules" name="position-10"/>
+						</div>
+					</div>
 				</div>
 			</div>
 			<?php
 			}
 			?>
-			<div class="row" id="contenu" name="contenu">
-				<br>
-				<hr>
+			<br>
+			<div class="row well" id="contenu" name="contenu">
 				<div class="col-lg-12">
 					<jdoc:include type="component" />
 				</div>
@@ -109,43 +128,60 @@ $sitename = $app->getCfg('sitename');
 			if ($menu->getActive() == $menu->getDefault()) {
 			?>
 			<div class="row" id="u6u9" name="u6u9">
-				<br>
-				<h1>U6 U9</h1>
-				<hr>
-				<div class="col-lg-12">
-					<jdoc:include type="modules" name="moduleu17bis" />
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title">U6 U9</h3>
+					</div>
+					<div class="panel-body">
+						<jdoc:include type="modules" name="moduleu17" />
+					</div>
 				</div>
 			</div>
 			<div class="row" id="u11" name="u11">
-				<br>
-				<h1>U11</h1>
-				<hr>
-				<div class="col-lg-12">
-					<jdoc:include type="modules" name="moduleu17" />
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title">U11</h3>
+					</div>
+					<div class="panel-body">
+						<jdoc:include type="modules" name="moduleu17" />
+					</div>
 				</div>
 			</div>
 			<div class="row" id="u13u15" name="u13u15">
-				<br>
-				<h1>U13 U15</h1>
-				<hr>
-				<div class="col-lg-12">
-					<jdoc:include type="modules" name="moduleu17" />
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title">U13 U15</h3>
+					</div>
+					<div class="panel-body">
+						<jdoc:include type="modules" name="moduleu17" />
+					</div>
 				</div>
 			</div>
 			<div class="row" id="u17" name="u17">
-				<br>
-				<h1>U17</h1>
-				<hr>
-				<div class="col-lg-12">
-					<jdoc:include type="modules" name="moduleu17" />
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title">U17</h3>
+					</div>
+					<div class="panel-body">
+						<jdoc:include type="modules" name="moduleu17" />
+					</div>
 				</div>
 			</div>
 			<div class="row" id="seniors" name="seniors">
-				<br>
-				<h1>S&#233;niors</h1>
-				<hr>
-				<div class="col-lg-12">
-					<jdoc:include type="modules" name="moduleu17" />
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title">S&#233;niors</h3>
+					</div>
+					<div class="panel-body">
+						<jdoc:include type="modules" name="moduleu17" />
+					</div>
+				</div>
+			</div>
+			<div class="row" id="footer" name="footer">
+				<div class="panel panel-default well">
+					<div class="panel-body">
+						<a href="http://www.joomla.org">Joomla!</a> est un Logiciel Libre diffusé sous licence <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU General Public</a>
+					</div>
 				</div>
 			</div>
 			<?php
